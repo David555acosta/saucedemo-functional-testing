091fd81dd1de2a61d6bb31683d28420681f5b7fb
David Acosta <davidisrraelacostaz.9@gmail.com>
2026-06-24 16:04:16 -0300

    corrigiendo errores en lista donde guardo productos añadidos

--- caso2.py
+++ caso2.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import unittest
 
@@ -24,7 +26,9 @@ class casoDePrueba1(unittest.TestCase):
     def test_buscar(self):
         driver = self.driver
         driver.get("https://www.saucedemo.com/")
+        productosAñadidos = []
         
+        #Iniciar sesion
         nombre = driver.find_element(By.XPATH, "//*[@id='user-name']")
         contraseña = driver.find_element(By.XPATH, "//*[@id='password']")
         btnIngresar = driver.find_element(By.NAME, "login-button")
@@ -38,16 +42,58 @@ class casoDePrueba1(unittest.TestCase):
         
         btnIngresar.click()
         
+        time.sleep(2)
+        
+        articuloA = driver.find_element(By.XPATH, "//*[@id='item_4_title_link']")
+        nombreArticuloA = articuloA.text
+        articuloA.click()
+        
+        time.sleep(2)
+        
+        btnAgregarArticulo = driver.find_element(By.NAME , "add-to-cart")
+        btnAgregarArticulo.click()
+        productosAñadidos.append(nombreArticuloA)
+        
+        time.sleep(2)
+        
+        btnVolverCatalogo = driver.find_element(By.NAME , "back-to-products")
+        btnVolverCatalogo.click()
+        
+        
+        time.sleep(2)
+        
+        articuloB = driver.find_element(By.XPATH, "//*[@id='item_3_title_link']")
+        articuloB.click()
+        nombreArticuloB = articuloB.text
+        productosAñadidos.append(nombreArticuloB)
+        
+        time.sleep(2)
+        
+        btnAgregarArticulo = driver.find_element(By.NAME , "add-to-cart")
+        btnAgregarArticulo.click()
+        
+        
+        time.sleep(2)
+        
+        btnVolverCatalogo = driver.find_element(By.NAME , "back-to-products")
+        btnVolverCatalogo.click()
+        
+        time.sleep(2)
+        
+        carrito = driver.find_element(By.XPATH , "//*[@id='shopping_cart_container']")
+        carrito.click()
+        
         time.sleep(5)
         
-        
-        referenciaLoginExitoso = driver.find_element(By.XPATH, "//*[@id='header_container']")
-        
-        time.sleep(5)
-        
-        self.assertTrue(referenciaLoginExitoso.is_displayed())
-        
-        
+        items = driver.find_elements(By.XPATH, '//*[@data-test="inventory-item"]')
+
+        nombres_carrito = []
+
+        for item in items:
+            nombre = item.find_element(By.XPATH, './/*[@data-test="inventory-item-name"]').text
+            nombres_carrito.append(nombre)
+            
+        self.assertEqual(set(productosAñadidos), set(nombres_carrito))
         
         
         
